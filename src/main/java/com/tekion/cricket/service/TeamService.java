28f359971c640a3d6253f7a9853b5a6b37aa2d09
Roshan Kumar <rk722579@gmail.com>
2022-02-22 11:50:39 +0530

    Added Match Summary and Scorecard Search By match Id

--- src/main/java/com/tekion/cricket/service/TeamService.java
+++ src/main/java/com/tekion/cricket/service/TeamService.java
@@ -56,28 +56,6 @@ public class TeamService {
             e.printStackTrace();
         }
 
-
-//        try {
-//            Scanner teamFile = new Scanner(file);
-//            if(teamFile.hasNextLine()){
-//                teamDetails.setTeamName(teamFile.nextLine());
-//            }
-//            int numberOfPlayer = 0;
-//            while(teamFile.hasNextLine()){
-//                PlayerDetails playerDetails = new PlayerDetails();
-//                playerDetails.setPlayerCode(numberOfPlayer);
-//                playerDetails.setPlayerName(teamFile.nextLine());
-//                playerDetails.setPlayerType(getPlayerTypeFromFile(teamFile.nextLine()));
-//                playerDetails.setBattingRating(Integer.parseInt(teamFile.nextLine()));
-//                playerDetails.setBowlingRating(Integer.parseInt(teamFile.nextLine()));
-//                playerDetailsList.add(playerDetails);
-//                numberOfPlayer++;
-//            }
-//        }
-//        catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        }
-
         teamDetails.setPlayersDetails(playerDetailsList);
         return teamDetails;
     }
